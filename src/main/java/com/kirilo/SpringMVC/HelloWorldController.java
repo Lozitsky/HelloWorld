7cd1a50acbc2211f6d26e1d11f77c86db598c1ed
Kirilo Lozitsky <bondxxl01@gmail.com>
2019-01-14 01:31:55 +0200

    Simple application HelloWorld on SpringMVC

---
+++ src/main/java/com/kirilo/SpringMVC/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.kirilo.SpringMVC;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+@Controller
+public class HelloWorldController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getHome(Locale locale, Model model){
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+        String format = dateFormat.format(date);
+        model.addAttribute("serverTime", format);
+        return "home";
+    }
+}
---
+++ web/WEB-INF/views/home.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Надія
+  Date: 14.01.2019
+  Time: 1:00
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Home Page</title>
+</head>
+<body>
+<h1>Hello People!</h1>
+
+<p>The time on the server is ${serverTime}.</p>
+
+</body>
+</html>
